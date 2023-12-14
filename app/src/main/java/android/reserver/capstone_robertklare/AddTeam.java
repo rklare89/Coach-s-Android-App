4b4ec4c84c67cf1af8ab85b32d71ea3d12e50960
rklare89 <142181787+rklare89@users.noreply.github.com>
2023-12-14 15:49:11 -0600

    Styles Main page and adds activity for adding teams.

---
+++ app/src/main/java/android/reserver/capstone_robertklare/AddTeam.java
@@ -0,0 +1,14 @@
+package android.reserver.capstone_robertklare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTeam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_team);
+    }
+}
